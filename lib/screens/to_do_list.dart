0a475645a946053428217f0e64144cb685a9a017
filipe caldas <filipe.ruas@hotmail.com>
2021-05-24 18:45:32 -0300

    refactor: to _do_list

--- lib/screens/to_do_list.dart
+++ lib/screens/to_do_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:its_time/components/loading.dart';
 import 'package:its_time/database/dao/to_do_dao.dart';
 import 'package:its_time/models/ToDo.dart';
 import 'package:its_time/screens/to_do_form.dart';
@@ -27,13 +28,7 @@ class _ToDoListState extends State<ToDoList> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context)
-              .push(
-                MaterialPageRoute(
-                  builder: (context) => ToDoForm(widget.idTicket),
-                ),
-              )
-              .then((value) => setState(() {}));
+          pushRoute(ToDoForm(widget.idTicket));
         },
         child: const Icon(Icons.add),
       ),
@@ -41,66 +36,76 @@ class _ToDoListState extends State<ToDoList> {
         initialData: List.empty(),
         future: _toDoDao.findAllWithTicketId(widget.idTicket),
         builder: (context, snapshot) {
-          List<ToDo> toDos = snapshot.data;
           if (snapshot.connectionState == ConnectionState.none &&
               snapshot.hasData == null) {
             print('project snapshot data is: ${snapshot.data}');
             return Text('Erro desconhecido');
           } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: Column(
-                children: [
-                  CircularProgressIndicator(),
-                  Text("Carregando"),
-                ],
-              ),
-            );
+            return loadingScreen();
           }
-          return ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (BuildContext context, int index) {
-              ToDo toDo = toDos[index];
-              return Card(
-                key: Key(toDo.id.toString()),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Row>[
-                    Row(
-                      children: <Widget>[
-                        Checkbox(
-                          value: toDo.conclusion,
-                          onChanged: (newValue) {
-                            setState(() {
-                              toDo.conclusion = newValue;
-                            });
-                            _toDoDao.update(toDo);
-                          },
-                        ),
-                        Text(toDo.title),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            debugPrint("Botão edit apertado");
-                          },
-                          child: Icon(Icons.edit),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            debugPrint("Botão delete apertado");
-                          },
-                          child: Icon(Icons.delete),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
+          return listViewBuild(snapshot);
         },
+      ),
+    );
+  }
+
+  void pushRoute(Object screen) {
+    Navigator.of(context)
+        .push(
+          MaterialPageRoute(
+            builder: (context) => screen,
+          ),
+        )
+        .then((value) => setState(() {}));
+  }
+
+  Widget listViewBuild(AsyncSnapshot snapshot) {
+    return ListView.builder(
+      itemCount: snapshot.data.length,
+      itemBuilder: (BuildContext context, int index) {
+        ToDo toDo = snapshot.data[index];
+        return eachToDo(toDo);
+      },
+    );
+  }
+
+  Widget eachToDo(ToDo toDo) {
+    return Card(
+      key: Key(toDo.id.toString()),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Row>[
+          Row(
+            children: <Widget>[
+              Checkbox(
+                value: toDo.conclusion,
+                onChanged: (newValue) {
+                  setState(() {
+                    toDo.conclusion = newValue;
+                  });
+                  _toDoDao.update(toDo);
+                },
+              ),
+              Text(toDo.title),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  debugPrint("Botão edit apertado");
+                },
+                child: Icon(Icons.edit),
+              ),
+              TextButton(
+                onPressed: () {
+                  debugPrint("Botão delete apertado");
+                },
+                child: Icon(Icons.delete),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

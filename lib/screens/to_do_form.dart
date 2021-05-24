a9c3837f654f71d5b3d8fe8fa84623bef3018afb
filipe caldas <filipe.ruas@hotmail.com>
2021-05-24 20:16:30 -0300

    refactor: to_do_form

--- lib/screens/to_do_form.dart
+++ lib/screens/to_do_form.dart
@@ -94,134 +94,23 @@ class _ToDoFormState extends State<ToDoForm> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          final String title = _controllerTitle.text;
-          final String description = _controllerDescription.text;
-          print(title);
-          print(description);
-
-          final String strWeekly = transformStrWeekly();
-
-          final ToDo newToDo = ToDo(
-              title: title,
-              description: description,
-              conclusion: false,
-              alert: isSwitchedAlert,
-              alertDate: strDateAlart,
-              alertTime: strTimeAlart,
-              previous: isSwitchedPrevious,
-              previousDate: strDatePrevious,
-              previousTime: strTimePrevious,
-              repeat: selectedRadio,
-              repeatWeekly: strWeekly,
-              alarm: isSwitchedAlarm,
-              idTicket: widget.idTicket);
-          _toDoDao.create(newToDo).then((id) => {
-                Navigator.pop(context),
-              });
+          onCreateToDo();
         },
         child: const Icon(Icons.check),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: _controllerTitle,
-              decoration: InputDecoration(
-                labelText: 'Tarefa',
-              ),
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
-            ),
-            TextField(
-              controller: _controllerDescription,
-              decoration: InputDecoration(
-                labelText: 'Notas',
-              ),
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
-              keyboardType: TextInputType.multiline,
-              minLines: 1,
-              maxLines: null,
-            ),
-            alertTask(),
-          ],
-        ),
-      ),
+      body: form(),
     );
   }
 
-  alertTask() {
-    return Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Switch(
-                value: isSwitchedAlert,
-                onChanged: (value) {
-                  setState(() {
-                    isSwitchedAlert = value;
-                  });
-                }),
-            Text("Lembrar"),
-          ],
-        ),
-        Offstage(
-          offstage: !isSwitchedAlert,
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                buttonsDateTimeAlert(context),
-                Row(
-                  children: <Widget>[
-                    Switch(
-                        value: isSwitchedPrevious,
-                        onChanged: (value) {
-                          setState(() {
-                            isSwitchedPrevious = value;
-                          });
-                        }),
-                    Text("Avisar com antecedência"),
-                  ],
-                ),
-                Offstage(
-                  offstage: !isSwitchedPrevious,
-                  child: buttonsDateTimePrevious(context),
-                ),
-                Row(
-                  children: <Widget>[
-                    Switch(
-                        value: isSwitchedRepeat,
-                        onChanged: (value) {
-                          setState(() {
-                            isSwitchedRepeat = value;
-                          });
-                        }),
-                    Text("Ativar repetição"),
-                  ],
-                ),
-                Offstage(
-                  offstage: !isSwitchedRepeat,
-                  child: buttonsRadioListRepeat(context),
-                ),
-                Row(
-                  children: <Widget>[
-                    Switch(
-                        value: isSwitchedAlarm,
-                        onChanged: (value) {
-                          setState(() {
-                            isSwitchedAlarm = value;
-                          });
-                        }),
-                    Text("Ativar alarme"),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+  Widget form() {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          inputToDoForm('Tarefa', _controllerTitle),
+          largeInputToDoForm('Notas', _controllerDescription),
+          alertButton(),
+        ],
+      ),
     );
   }
 
@@ -306,9 +195,87 @@ class _ToDoFormState extends State<ToDoForm> {
   Widget buttonsRadioListRepeat(BuildContext context) {
     return Column(
       children: <Widget>[
+        radioButtonRepeat('diariamente', 1),
+        radioButtonRepeatWeekly(),
+        radioButtonRepeat('mensalmente', 3),
+        radioButtonRepeat('anualmente', 4),
+      ],
+    );
+  }
+
+  void onCreateToDo() {
+    final String title = _controllerTitle.text;
+    final String description = _controllerDescription.text;
+
+    final String strWeekly = transformStrWeekly();
+
+    final ToDo newToDo = ToDo(
+        title: title,
+        description: description,
+        conclusion: false,
+        alert: isSwitchedAlert,
+        alertDate: strDateAlart,
+        alertTime: strTimeAlart,
+        previous: isSwitchedPrevious,
+        previousDate: strDatePrevious,
+        previousTime: strTimePrevious,
+        repeat: selectedRadio,
+        repeatWeekly: strWeekly,
+        alarm: isSwitchedAlarm,
+        idTicket: widget.idTicket);
+    _toDoDao.create(newToDo).then((id) => {
+          Navigator.pop(context),
+        });
+  }
+
+  Widget inputToDoForm(String label, TextEditingController controller) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+      ),
+      style: TextStyle(
+        fontSize: 18.0,
+      ),
+    );
+  }
+
+  Widget largeInputToDoForm(String label, TextEditingController controller) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+      ),
+      style: TextStyle(
+        fontSize: 18.0,
+      ),
+      keyboardType: TextInputType.multiline,
+      minLines: 1,
+      maxLines: null,
+    );
+  }
+
+  Widget alarmButton() {
+    return Row(
+      children: <Widget>[
+        Switch(
+            value: isSwitchedAlarm,
+            onChanged: (value) {
+              setState(() {
+                isSwitchedAlarm = value;
+              });
+            }),
+        Text("Ativar alarme"),
+      ],
+    );
+  }
+
+  Widget radioButtonRepeatWeekly() {
+    return Column(
+      children: [
         RadioListTile(
-          title: Text("diariamente"),
-          value: 1,
+          title: Text("semanalmente"),
+          value: 2,
           groupValue: selectedRadio,
           onChanged: (value) {
             setState(
@@ -318,158 +285,237 @@ class _ToDoFormState extends State<ToDoForm> {
             );
           },
         ),
-        Column(
+        Offstage(
+          offstage: isSelectedWeekly(selectedRadio),
+          child: Row(
+            children: <Widget>[
+              checkboxSunday(),
+              checkboxMonday(),
+              checkboxTuesday(),
+              checkboxWednesday(),
+              checkboxThursday(),
+              checkboxFriday(),
+              checkboxSaturday(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget radioButtonRepeat(String label, int radioValue) {
+    return RadioListTile(
+      title: Text(label),
+      value: radioValue,
+      groupValue: selectedRadio,
+      onChanged: (value) {
+        setState(
+          () {
+            selectedRadio = value;
+          },
+        );
+      },
+    );
+  }
+
+  Widget previousButton() {
+    return Column(
+      children: [
+        Row(
           children: <Widget>[
-            RadioListTile(
-              title: Text("semanalmente"),
-              value: 2,
-              groupValue: selectedRadio,
-              onChanged: (value) {
-                setState(
-                  () {
-                    selectedRadio = value;
-                  },
-                );
-              },
-            ),
-            Offstage(
-              offstage: isSelectedWeekly(selectedRadio),
-              child: Row(
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Checkbox(
-                        value: isCheckedSunday,
-                        onChanged: (value) {
-                          setState(
-                            () {
-                              isCheckedSunday = value;
-                            },
-                          );
-                        },
-                      ),
-                      Text("Domingo")
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Checkbox(
-                        value: isCheckedMonday,
-                        onChanged: (value) {
-                          setState(
-                            () {
-                              isCheckedMonday = value;
-                            },
-                          );
-                        },
-                      ),
-                      Text("Segunda")
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Checkbox(
-                        value: isCheckedTuesday,
-                        onChanged: (value) {
-                          setState(
-                            () {
-                              isCheckedTuesday = value;
-                            },
-                          );
-                        },
-                      ),
-                      Text("Terça")
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Checkbox(
-                        value: isCheckedWednesday,
-                        onChanged: (value) {
-                          setState(
-                            () {
-                              isCheckedWednesday = value;
-                            },
-                          );
-                        },
-                      ),
-                      Text("Quarta")
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Checkbox(
-                        value: isCheckedThursday,
-                        onChanged: (value) {
-                          setState(
-                            () {
-                              isCheckedThursday = value;
-                            },
-                          );
-                        },
-                      ),
-                      Text("Quinta")
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Checkbox(
-                        value: isCheckedFriday,
-                        onChanged: (value) {
-                          setState(
-                            () {
-                              isCheckedFriday = value;
-                            },
-                          );
-                        },
-                      ),
-                      Text("Sexta")
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Checkbox(
-                        value: isCheckedSaturday,
-                        onChanged: (value) {
-                          setState(
-                            () {
-                              isCheckedSaturday = value;
-                            },
-                          );
-                        },
-                      ),
-                      Text("Sábado")
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            RadioListTile(
-              title: Text("mensalmente"),
-              value: 3,
-              groupValue: selectedRadio,
-              onChanged: (value) {
-                setState(
-                  () {
-                    selectedRadio = value;
-                  },
-                );
-              },
-            ),
-            RadioListTile(
-              title: Text("anualmente"),
-              value: 4,
-              groupValue: selectedRadio,
-              onChanged: (value) {
-                setState(
-                  () {
-                    selectedRadio = value;
-                  },
-                );
-              },
-            ),
+            Switch(
+                value: isSwitchedPrevious,
+                onChanged: (value) {
+                  setState(() {
+                    isSwitchedPrevious = value;
+                  });
+                }),
+            Text("Avisar com antecedência"),
           ],
         ),
+        Offstage(
+          offstage: !isSwitchedPrevious,
+          child: buttonsDateTimePrevious(context),
+        ),
+      ],
+    );
+  }
+
+  Widget repeatButton() {
+    return Column(
+      children: [
+        Row(
+          children: <Widget>[
+            Switch(
+                value: isSwitchedRepeat,
+                onChanged: (value) {
+                  setState(() {
+                    isSwitchedRepeat = value;
+                  });
+                }),
+            Text("Ativar repetição"),
+          ],
+        ),
+        Offstage(
+          offstage: !isSwitchedRepeat,
+          child: buttonsRadioListRepeat(context),
+        ),
+      ],
+    );
+  }
+
+  Widget alertButton() {
+    return Column(
+      children: [
+        Row(
+          children: <Widget>[
+            Switch(
+                value: isSwitchedAlert,
+                onChanged: (value) {
+                  setState(() {
+                    isSwitchedAlert = value;
+                  });
+                }),
+            Text("Lembrar"),
+          ],
+        ),
+        Offstage(
+          offstage: !isSwitchedAlert,
+          child: Column(
+            children: <Widget>[
+              buttonsDateTimeAlert(context),
+              previousButton(),
+              repeatButton(),
+              alarmButton(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget checkboxSunday() {
+    return Column(
+      children: <Widget>[
+        Checkbox(
+          value: isCheckedSunday,
+          onChanged: (newValue) {
+            setState(
+              () {
+                isCheckedSunday = newValue;
+              },
+            );
+          },
+        ),
+        Text('Domingo')
+      ],
+    );
+  }
+
+  Widget checkboxMonday() {
+    return Column(
+      children: <Widget>[
+        Checkbox(
+          value: isCheckedMonday,
+          onChanged: (newValue) {
+            setState(
+              () {
+                isCheckedMonday = newValue;
+              },
+            );
+          },
+        ),
+        Text("Segunda")
+      ],
+    );
+  }
+
+  Widget checkboxTuesday() {
+    return Column(
+      children: <Widget>[
+        Checkbox(
+          value: isCheckedTuesday,
+          onChanged: (newValue) {
+            setState(
+              () {
+                isCheckedTuesday = newValue;
+              },
+            );
+          },
+        ),
+        Text("Terça")
+      ],
+    );
+  }
+
+  Widget checkboxWednesday() {
+    return Column(
+      children: <Widget>[
+        Checkbox(
+          value: isCheckedWednesday,
+          onChanged: (newValue) {
+            setState(
+              () {
+                isCheckedWednesday = newValue;
+              },
+            );
+          },
+        ),
+        Text("Quarta")
+      ],
+    );
+  }
+
+  Widget checkboxThursday() {
+    return Column(
+      children: <Widget>[
+        Checkbox(
+          value: isCheckedThursday,
+          onChanged: (newValue) {
+            setState(
+              () {
+                isCheckedThursday = newValue;
+              },
+            );
+          },
+        ),
+        Text("Quinta")
+      ],
+    );
+  }
+
+  Widget checkboxFriday() {
+    return Column(
+      children: <Widget>[
+        Checkbox(
+          value: isCheckedFriday,
+          onChanged: (newValue) {
+            setState(
+              () {
+                isCheckedFriday = newValue;
+              },
+            );
+          },
+        ),
+        Text("Sexta")
+      ],
+    );
+  }
+
+  Widget checkboxSaturday() {
+    return Column(
+      children: <Widget>[
+        Checkbox(
+          value: isCheckedSaturday,
+          onChanged: (newValue) {
+            setState(
+              () {
+                isCheckedSaturday = newValue;
+              },
+            );
+          },
+        ),
+        Text("Sábado")
       ],
     );
   }
